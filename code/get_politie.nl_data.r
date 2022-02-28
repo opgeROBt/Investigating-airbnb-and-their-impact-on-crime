bff20c09ea7c8bf75d97d831fd73fe256aed2533
opgeROBt <rob.esenkbrink@gmail.com>
2022-02-28 14:52:37 +0100

    changed R code to work on the politie API

--- code/get_politie.nl_data.r
+++ code/get_politie.nl_data.r
@@ -1,9 +1,8 @@
-
 ## information regarding dataset 
 **police open data:**  https://data.politie.nl/portal.html?_la=nl&_catalog=Politie&tableId=47022NED&_theme=97
 **example data:** https://data.politie.nl/#/Politie/nl/dataset/47022NED/table?ts=1645109405238 
-
-
+  
+  
 # Run once:
 install.packages("cbsodataR")
 library(cbsodataR)
@@ -13,11 +12,14 @@ library(cbsodataR)
 toc <- cbs_get_toc()
 head(toc)
 
+catalogs <-  cbs_get_catalogs()
+View(catalogs) 
 
 # Downloading entire dataset (can take up to 30s)
-data <- cbs_get_data("47022NED")
+data <- cbs_get_data("47022NED", catalog = "politie")
 head(data)
 
+?cbsodataR
 
 # Downloading metadata
 metadata <- cbs_get_meta("47022NED")
@@ -25,7 +27,7 @@ head(metadata)
 
 
 # Downloading a subset
-data <- cbs_get_data("47022NED", 
-             WijkenEnBuurten = "GM0363    ",
-             select = c("WijkenEnBuurten", "Perioden",	"SoortMisdrijf"))
+data <- cbs_get_data("47022NED", catalog = "politie",
+                     perioden = "2021MM12", 
+                     select = c("WijkenEnBuurten", "Perioden",	"SoortMisdrijf"))
 head(data)
